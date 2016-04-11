e51c4d81ea5db3896bf7f23f3602fe342d42a0e8
Gordondalos <Gordondalos@gmail.com>
2016-04-11 16:39:40 +0600

    Начал переделку главной страницы

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new ErpBundle\ErpBundle(),
+            new SiteBundle\SiteBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
