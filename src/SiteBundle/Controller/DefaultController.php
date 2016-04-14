3479e486a05e76aeb4d0dfa8ac9128d889db5bc4
Gordondalos <Gordondalos@gmail.com>
2016-04-14 22:18:07 +0600

    Делаю портфолио

--- src/SiteBundle/Controller/DefaultController.php
+++ src/SiteBundle/Controller/DefaultController.php
@@ -16,14 +16,44 @@ class DefaultController extends Controller
         return $this->render('SiteBundle:Default:customers.html.twig');
     }
 
+    public function digitalAction()
+    {
+        return $this->render('SiteBundle:Portfolio:digital.html.twig');
+    }
+
+    public function ereportAction()
+    {
+        return $this->render('SiteBundle:Portfolio:ereport.html.twig');
+    }
+
     public function partnersAction()
     {
         return $this->render('SiteBundle:Default:partners.html.twig');
     }
 
+    public function perilaAction()
+    {
+        return $this->render('SiteBundle:Portfolio:perila.html.twig');
+    }
+
     public function portfolioAction()
     {
         return $this->render('SiteBundle:Default:portfolio.html.twig');
+    }
+
+    public function seAction()
+    {
+        return $this->render('SiteBundle:Portfolio:se.html.twig');
+    }
+
+    public function superlativeAction()
+    {
+        return $this->render('SiteBundle:Portfolio:superlative.html.twig');
+    }
+
+    public function tandemAction()
+    {
+        return $this->render('SiteBundle:Portfolio:tandem.html.twig');
     }
 
     public function colleagueAction()
@@ -47,5 +77,10 @@ class DefaultController extends Controller
     public function baltspecservisAction()
     {
         return $this->render('SiteBundle:Portfolio:baltspecservise.html.twig');
+    }
+
+    public function cdiAction()
+    {
+        return $this->render('SiteBundle:Portfolio:cdi.html.twig');
     }
 }
